aa88e311a9cbca0458b89a7eda73a273f46ed45b
MD JAHANGIR ALAM <jahangiralam85860@gmail.com>
2024-06-10 23:07:18 +0600

    fundamentals js

---
+++ Fundamentals-js.js
@@ -0,0 +1,24 @@
+let a = 10;
+let b = 5;
+
+// Arithmetic Operations 
+console.log(a + b); // 15
+console.log(a - b); // 5
+console.log(a * b); // 50
+console.log(a / b); // 2
+console.log(a % b); // 0
+
+// Comparison Operations
+console.log(a > b); // true
+console.log(a < b); // false
+console.log(a == b); // false
+console.log(a != b); // true
+
+// Logical Opetions
+let isAdult = true;
+let hasPermission = false;
+
+console.log(isAdult && hasPermission); // false
+console.log(isAdult || hasPermission); // true
+console.log(!isAdult); // false
+
